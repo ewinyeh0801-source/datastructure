05f72163a00a0db15c688b7a31f4a76c0d7d3289
ewinyeh0801-source <ewinyeh0801@gmail.com>
2026-09-22 16:20:01 +0800

    0922

--- first.js
+++ first.js
@@ -1,17 +1,30 @@
 var weight=50;
 var height=160;
+
+var readline = require("readline-sync");
+
+while(true){
+var weight = readline.question('your weight (10~200 kg)?');
+if(weight < 10 && weight > 200)
+  continue;
+else
+    break;
+  }
+
+
 var bmi = weight/((height/100)**2);
-console.log("your MI:"+ bmi);
-var weight = 60; 
-var height = 160;
+console.log("your BMI:"+ bmi);
+
+
  var bmi = weight / (height / 100 ** 2);
  console.log("Your BMI:" + bmi); 
+
  function test(weight=70)
  {    
-     var weight=60;     
+     var weight=62;     
      console.log("weight="+weight)   
        return weight;
      }  
 
-     test(80); 
+     var resuit=test(80); 
       console.log("weight="+weight)
